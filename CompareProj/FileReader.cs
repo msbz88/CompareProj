8e2f98e902da5c285906ad3d4f75b61c51602cd8
8msbz8 <8msbz8@gmail.com>
2018-03-01 17:00:45 +0200

    Check 2

--- CompareProj/FileReader.cs
+++ CompareProj/FileReader.cs
@@ -1,27 +1,71 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.IO;
-using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CompareProj {
     public class FileReader {
-        public static DataTable ReadFileToTable(string path, char delimiter) {
-            DataTable table = new DataTable();
+        public static DataTable ReadFileToTable(string path, char delimiter, DataTable dataTable) {
+            string[] AllLines = null;
+            int max = File.ReadLines(path).Count();
 
             using (StreamReader steamReader = new StreamReader(path)) {
                 string[] headers = steamReader.ReadLine().Split(delimiter);
                 foreach (string header in headers) {
-                    table.Columns.Add(header);
-                }
-                while (!steamReader.EndOfStream) {
-                    string[] rows = steamReader.ReadLine().Split(delimiter);
-                    DataRow dr = table.NewRow();
-                    for (int i = 0; i < headers.Length; i++) {
-                        dr[i] = rows[i];
-                    }
-                    table.Rows.Add(dr);
+                    dataTable.Columns.Add(header);
                 }
             }
-            return table;
+
+            AllLines = new string[max];
+            AllLines = File.ReadAllLines(path);
+            AllLines = AllLines.Skip(1).ToArray();
+
+            foreach (var item in AllLines) {
+                DataRow newRow = dataTable.NewRow();
+                newRow[0] = item.ToString();
+                dataTable.Rows.Add(newRow);
+            }
+
+            /*
+            while (!steamReader.EndOfStream) {
+                string[] rows = steamReader.ReadLine().Split(delimiter);
+                DataRow newRow = dataTable.NewRow();
+                for (int i = 0; i < headers.Length; i++) {
+                    newRow[i] = rows[i];
+                }
+                dataTable.Rows.Add(newRow);
+            }
+            */
+
+            return dataTable;
+        }
+
+        public static DataTable GetDataSourceFromFile(string path, char delimiter, DataTable dataTable) {
+            string[] columns = null;
+
+            var lines = File.ReadAllLines(path);
+
+            // assuming the first row contains the columns information
+            if (lines.Count() > 0) {
+                columns = lines[0].Split(new char[] { delimiter });
+
+                foreach (var column in columns)
+                    dataTable.Columns.Add(column);
+            }
+
+            // reading rest of the data
+            for (int i = 1; i < lines.Count(); i++) {
+                DataRow dr = dataTable.NewRow();
+                string[] values = lines[i].Split(new char[] { delimiter });
+
+                for (int j = 0; j < values.Count() && j < columns.Count(); j++)
+                    dr[j] = values[j];
+
+                dataTable.Rows.Add(dr);
+            }
+            Console.WriteLine("File loaded");
+            return dataTable;
         }
     }
 }
--- CompareProj/Program.cs
+++ CompareProj/Program.cs
@@ -1,21 +1,44 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 
 namespace CompareProj {
     class Program {
+        public static void PrintDataTable(DataTable dataTable) {
+            DataRow[] currentRows = dataTable.Select(null, null, DataViewRowState.CurrentRows);
+            if (currentRows.Length < 1)
+                Console.WriteLine("No Current Rows Found");
+            else {
+                foreach (DataColumn column in dataTable.Columns)
+                    Console.Write("\t{0}", column.ColumnName);
+
+                Console.WriteLine("\tRowState");
+
+                foreach (DataRow row in currentRows) {
+                    foreach (DataColumn column in dataTable.Columns)
+                        Console.Write("\t{0}", row[column]);
+
+                    Console.WriteLine("\t" + row.RowState);
+                }
+            }
+        }
+
         static void Main() {
-            string masterFile = @"C:\Users\msbz\Desktop\CompareTest\[VT_FRMLVALUES_00.txt";
-            string testFile = @"C:\Users\msbz\Desktop\CompareTest\]VT_FRMLVALUES_00.txt";
+            string masterFile = @"C:\Users\msbz\Desktop\CompareTest\[SD_KEYRATIOS_00.txt";
+            string testFile = @"C:\Users\msbz\Desktop\CompareTest\]SD_KEYRATIOS_00.txt";
             string comparedFile = @"C:\Users\msbz\Desktop\CompareTest\CompareResult.xml";
             char delimiter = '\t';
-            DataTable master = new DataTable();
-            DataTable test = new DataTable();
-            DataTable compare = new DataTable();
+            DataTable master = new DataTable("Master");
+            DataTable test = new DataTable("Test");
 
-            master = FileReader.ReadFileToTable(masterFile, delimiter);
-            test = FileReader.ReadFileToTable(testFile, delimiter);
-            Compare forCompare = new Compare(master, test);
-            forCompare.ExecuteComparison();
-            compare.WriteXml(comparedFile);
+            FileReader.GetDataSourceFromFile(masterFile, delimiter, master);
+            FileReader.GetDataSourceFromFile(testFile, delimiter, test);
+
+            //PrintDataTable(test);
+
+
+
+            //Compare forCompare = new Compare(master, test);
+            //forCompare.ExecuteComparison().WriteXml(comparedFile);
         }
     }
 }
